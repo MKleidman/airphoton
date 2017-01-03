924f02e968ca6aff1b7aaedf8d2bb06e4bde3742
Matis Kleidman <matis@socialcodeinc.com>
2017-01-03 13:22:42 -0500

    login/logout

--- airphoton/views.py
+++ airphoton/views.py
@@ -1,5 +1,9 @@
+import csv
+import os.path
 from django.shortcuts import render, render_to_response
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.conf import settings
 
 def ping(request):
     "Health check for determining if the server is available"
@@ -9,3 +13,9 @@ def ping(request):
 @login_required()
 def home(request):
 	return render(request, 'home.html')
+
+@login_required()
+def loaddata(request):
+    f = open(os.path.join(settings.STATIC_ROOT, 'static/data/', request.POST['filename']))
+    response =  "\n".join([",".join(line) for line in csv.reader(f)])
+    return HttpResponse(response, content_type='text/csv')
